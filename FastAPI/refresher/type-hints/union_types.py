fab451343f1c09588b5a61b404f890b953b7577e
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-02 15:05:50 +0545

    demo: bug while comparing dtype with isinstance

--- FastAPI/refresher/type-hints/union_types.py
+++ FastAPI/refresher/type-hints/union_types.py
@@ -8,3 +8,10 @@ def student_identifier(id:Union[int,str],active:Union[bool,str]):
     return {
         "ID":id,"Active":active
     }
+
+@app.get("/student/score")
+def student_score(score: int|str):
+    if isinstance(score,str):
+        return {"Numeric Score":score}
+    else:
+        return {"Grade Letter":score}
