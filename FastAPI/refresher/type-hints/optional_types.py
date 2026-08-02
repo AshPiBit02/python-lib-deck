1309c6086f2ebc2704106641fe264262180e6e1f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-02 14:45:04 +0545

    demo: usage 1

--- FastAPI/refresher/type-hints/optional_types.py
+++ FastAPI/refresher/type-hints/optional_types.py
@@ -8,3 +8,9 @@ def get_student(name:str,nickname:Optional[str]=None):
     return{
         "Name":name,"Nickname":nickname if nickname else "No nickname provided"
     }
+
+@app.get("/student/details/")
+def get_details(name:str,age:Optional[int]=None):
+    return{
+        "Name":name,"Age":age if age else "Age not provided"
+    }
