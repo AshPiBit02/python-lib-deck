3f923d6afd2c9590888e068c32215b0cc5a30f46
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-02 15:42:08 +0545

    demo: dict type return

--- FastAPI/refresher/type-hints/any_type.py
+++ FastAPI/refresher/type-hints/any_type.py
@@ -1,17 +1,16 @@
-# from fastapi import FastAPI
-# from typing import Any
-
-# app=FastAPI()
-
-# @app.get("/student/data")
-# def student_data(info:Any):
-#     return {"Received":info,"Type":str(type(info).__name__)}
-
 from fastapi import FastAPI
 from typing import Any
 
-app = FastAPI()
+app=FastAPI()
 
 @app.get("/student/data")
-def student_data(info: Any):
-    return {"Received": info, "Type": str(type(info).__name__)}
+def student_data(info:Any):
+    return {"Received":info,"Type":str(type(info).__name__)}
+
+
+@app.get("/student/record")
+def student_record(record:Any):
+    if isinstance(record,dict):
+        return {"Result":"Valid"}
+    else:
+        return {"Result":"Invalid"}
