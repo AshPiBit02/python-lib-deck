ceeaad99409cc4df1ec75c6aa87270c87401d35e
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-02 14:34:54 +0545

    demo: takes input from url and display data

--- FastAPI/refresher/type-hints/basic_types.py
+++ FastAPI/refresher/type-hints/basic_types.py
@@ -7,3 +7,11 @@ def get_student(id:int,name:str,passed:bool,gpa:float):
         "ID":id,"Name":name,"Passed":passed,"GPA":gpa
     }
 print(get_student(2005,"Aashish",True,3.97))
+
+
+@app.get("/student/{roll}/{name}/{attendance}")
+def get_student_attendance(roll:int,name:str,attendance:bool):
+    return {
+        "Roll no.":roll,"Name":name,"Attendance":attendance
+    }
+
