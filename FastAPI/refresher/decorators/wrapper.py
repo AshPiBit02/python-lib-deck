6d7df61ebec7ae435a8477673f7d8e259331aca9
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-04 14:28:28 +0545

    demo: decorator -> decorating functions that takes arguements

--- FastAPI/refresher/decorators/wrapper.py
+++ FastAPI/refresher/decorators/wrapper.py
@@ -10,5 +10,33 @@ def my_decorator(func):
 @my_decorator
 def say_hello():
     print("Hello!")
+say_hello
 
-say_hello()
+# Decorating functions that take arguments
+
+from typing import Callable,Any
+
+def logger(func:Callable)->Callable:
+    def wrapper(*args:Any,**kwargs:Any)->Any:
+        print(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+        result=func(*args,**kwargs)
+        print(f"{func.__name__} returned {result}")
+        return result
+    return wrapper
+
+@logger
+def add(a:int,b:int)->int:
+    return a+b
+add(4,5)
+
+@logger
+def greet(name:str,age:int)->str:
+    if age<18:
+        return f"Hello {name}, kiddo!"
+    else:
+        return f"Hello {name}, Sir!"
+
+print(greet("Aashish",21))
+
+
+
