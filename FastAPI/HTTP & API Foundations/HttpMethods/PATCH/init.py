b328b617de8c925b5b186319163c240965a84e9b
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-09 23:23:30 +0545

    feat: gives proper response for all updated fields

--- FastAPI/HTTP & API Foundations/HttpMethods/PATCH/init.py
+++ FastAPI/HTTP & API Foundations/HttpMethods/PATCH/init.py
@@ -16,23 +16,24 @@ class Product(BaseModel):
     price:float|None=None
     quantity:int|None=None
 
-
+message=[]
 @app.patch("/products/{product_id}")
 def update_product(product_id:int,product:Product):
     for existing_product in products:
         if existing_product["id"]==product_id:
             if product.name is not None:
                 existing_product["name"]=product.name
-                return {"message":f"Updated product name ({existing_product['name']} -> {product.name})"}
+                message.append({"Updated name":f"{existing_product['name']} -> {product.name}"})
             if product.categroy is not None:
                 existing_product["category"]=product.category
-                return {"message":f"Updated product category ({existing_product['category']} -> {product.category})"}
+                message.append({"Updated category":f"{existing_product['category']} -> {product.category}"})
             if product.price is not None:
                 existing_product["price"]=product.price
-                return {"message":f"Updated product price ({existing_product['price']} -> {product.price})"}
+                message.append({"Updated price":f"{existing_product['price']} -> {product.price}"})
             if product.quantity is not None:
                 existing_product["quantity"]=product.quantity
-                return {"message":f"Updated product quantity ({existing_product['quantity']} -> {product.quantity})"}
+                message.append({"Updated quantity":f"{existing_product['quantity']} -> {product.quantity}"})
+            return message
     raise HTTPException(status_code=404,detail=f"Product with id {product_id} not found!")
             
 @app.get("/products")
