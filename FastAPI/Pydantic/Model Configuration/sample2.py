66dcb14878a1643ee829df00f65e4af546d33926
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-13 21:35:00 +0545

    imple: use of ignore that ignores the unknown fields

--- FastAPI/Pydantic/Model Configuration/sample2.py
+++ FastAPI/Pydantic/Model Configuration/sample2.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel,ConfigDict
 class AppBaseModel(BaseModel):
     model_config=ConfigDict(
         str_strip_whitespace=True,
-        extra="forbid",
+        extra="ignore",
         str_to_lower=True
     )
 
@@ -15,7 +15,7 @@ class UserIn(AppBaseModel):
     username:str
     email:str
 
-book=BookIn(title="   Atomic Habbits   ",author="James Clear ")
+book=BookIn(title="   Atomic Habbits   ",author="James Clear ",price=156)
 user=UserIn(username="ashpibit02  ",email="   aashishchaudhari240@gmail.com")
 print(book)
 print(user)
