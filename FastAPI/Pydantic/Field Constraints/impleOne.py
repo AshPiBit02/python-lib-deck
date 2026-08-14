2704bbff8b39ce0b37ad91d032b9407a56c16a17
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-14 13:10:36 +0545

    imple: field constraints imple  with Annotated

--- FastAPI/Pydantic/Field Constraints/impleOne.py
+++ FastAPI/Pydantic/Field Constraints/impleOne.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel,Field,EmailStr,ConfigDict,ValidationError
+from typing import Annotated
 
+PositiveMultipleOfHalf=Annotated[float,Field(multiple_of=0.5,gt=0)]
 
 class Ticket(BaseModel):
     event_code:str=Field(...,min_length=3,max_length=8,pattern=r"^[A-Z]+[0-9]{2,4}$")
-    price:float=Field(...,gt=0,multiple_of=0.50
+    price:PositiveMultipleOfHalf
     quantity:int=Field(default=1,ge=1,le=10)
     model_config=ConfigDict(populate_by_name=True)
     purchaser_email:EmailStr=Field(...,alias="purchaseEmail")
