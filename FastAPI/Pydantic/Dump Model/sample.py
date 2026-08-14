7fa9ff2d74ebecead5efd748079dd69e352b07bb
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-14 10:37:59 +0545

    imple: dump model credentials

--- FastAPI/Pydantic/Dump Model/sample.py
+++ FastAPI/Pydantic/Dump Model/sample.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 class Book(BaseModel):
     title:str
-    author:str
+    author:str=None
     copies_available:int|None=None
 
 book=Book(title="Dune",author="Frank Herbert")
@@ -12,3 +12,6 @@ print(book.model_dump_json())
 print(book.model_dump(exclude={"copies_available","author"})) # drop specific fields
 print(book.model_dump(include={"title","author"}))
 print(book.model_dump(exclude_unset=True)) # only fields the CLIENT actually provided
+print(book.model_dump(exclude_none=True))
+# print(Book.model_dump(exclude_defaults=True)) exclude fields whose value equals the field's defaults
+print(book.model_dump(exclude_none=True)) # excludes fields whose value is None
