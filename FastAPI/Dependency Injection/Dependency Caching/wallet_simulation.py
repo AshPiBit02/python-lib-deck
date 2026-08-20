f6b4ad50e0c72121b7cdba923280b8ed08c756e8
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-20 18:16:22 +0545

    bug: unable to run due to the pin validation dependency error

--- FastAPI/Dependency Injection/Dependency Caching/wallet_simulation.py
+++ FastAPI/Dependency Injection/Dependency Caching/wallet_simulation.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,Header,HTTPException,Depends,APIRouter
 from typing import Annotated
 from dummy_data import USER_PRIVATE_CREDENITIALS
+app=FastAPI()
 
 
 current_user_log_status={"userid":None,"logged":False}
@@ -10,11 +11,14 @@ def user_auth(userid:str=Header(...),password:str=Header(...))->str:
         raise HTTPException(status_code=404,detail="Unknown user!")
     if USER_PRIVATE_CREDENITIALS[userid]["password"]!=password:
         raise HTTPException(status_code=401,detail="Incorrect password!")
-    current_user_log_status={"userid":userid,"logged":True}
+    current_user_log_status["userid"]=userid
+    current_user_log_status["logged"]=True
     return {"message":"Login successsful"}
 
 def validate_pin(pin:str=Header(...)):
     user_id=current_user_log_status["userid"]
+    if not user_id:
+        raise HTTPException(status_code=403,detail="No user logged in")
     if pin != USER_PRIVATE_CREDENITIALS[user_id]["pin"]:
         raise HTTPException(status_code=401,detail="Incorrect PIN")
     return {"message":"Transaction successful"}
@@ -22,7 +26,13 @@ def validate_pin(pin:str=Header(...)):
 pin_validation_dependency=Annotated(dict,Depends(validate_pin))
 login_user_dependency=Annotated(dict,Depends(user_auth))
 
-def user_logged()
+@app.get("/wallet/login")
+def login(login_user_dependency):
+    return {"message":"Welcome, Sir. Your current balance is $999"}
+
+
+
+
 
 
 
@@ -31,4 +41,3 @@ def user_logged()
 
 
     
-app=FastAPI()
