62dc6d1703f6630872fcdc846bbc5e05823d0084
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-20 19:05:41 +0545

    feat: add input amount validation and withdraw transaction endpoint refactor: improve dependency injection for user, PIN, and amount checks

--- FastAPI/Dependency Injection/Dependency Caching/wallet_simulation.py
+++ FastAPI/Dependency Injection/Dependency Caching/wallet_simulation.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Header,HTTPException,Depends,APIRouter
 from typing import Annotated
+from pydantic import field_validator
 from dummy_data import USER_PRIVATE_CREDENITIALS
 app=FastAPI()
 
@@ -17,7 +18,7 @@ def user_auth(userid:str=Header(...),password:str=Header(...))->dict:
 def get_user()->str:
     user=current_user_log_status["userid"]
     if not user:
-        raise HTTPException(status_code=403,detail="No user logged in")
+        raise HTTPException(status_code=403,detail="Login to wallet first!")
     return user
 
 user_dependency=Annotated[str,Depends(get_user)]
@@ -34,12 +35,24 @@ def validate_pin(user:user_dependency,pin:str=Header(...)):
         raise HTTPException(status_code=401,detail="Incorrect PIN")
     return {"message":"Transaction successful"}
 
+def inputAmount(amount:float=Header(...))->float:
+    if amount<=0.0:
+        raise HTTPException(status_code=400,detail="Amount must be non-negative")
+    return amount
+
 pin_validation_dependency=Annotated[dict,Depends(validate_pin)]
 login_user_dependency=Annotated[dict,Depends(user_auth)]
+input_amount_dependency=Annotated[float,Depends(inputAmount)]
 
 @app.get("/wallet/login")
 def login(res:login_user_dependency,user:user_dependency):
     return {"message":f"Welcome, Sir({user}). Your current balance is {USER_PRIVATE_CREDENITIALS[user]['balance']}$"}
 
 @app.get("/wallet/transaction/withdraw")
-def tranfer()
+def transfer(user:user_dependency,logged:user_logged_dependency,amount:input_amount_dependency):
+    current_balance=USER_PRIVATE_CREDENITIALS[user]["balance"]
+    if current_balance<amount:
+        raise HTTPException(status_code=400,detail="Insufficient Balance!")
+    new_balance=current_balance-amount
+    USER_PRIVATE_CREDENITIALS[user]["balance"]=new_balance
+    return {"message":f"{amount}$ withdrawn from {user}'s account successfully. Updated balance: {new_balance}$"}
