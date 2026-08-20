cc479b20e62782ccba440f35e1684559047cb050
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-20 18:36:49 +0545

    feat: feat: add login flow with user authentication, PIN validation, and balance display

--- FastAPI/Dependency Injection/Dependency Caching/wallet_simulation.py
+++ FastAPI/Dependency Injection/Dependency Caching/wallet_simulation.py
@@ -6,7 +6,7 @@ app=FastAPI()
 
 current_user_log_status={"userid":None,"logged":False}
 
-def user_auth(userid:str=Header(...),password:str=Header(...))->str:
+def user_auth(userid:str=Header(...),password:str=Header(...))->dict:
     if userid not in USER_PRIVATE_CREDENITIALS:
         raise HTTPException(status_code=404,detail="Unknown user!")
     if USER_PRIVATE_CREDENITIALS[userid]["password"]!=password:
@@ -14,12 +14,21 @@ def user_auth(userid:str=Header(...),password:str=Header(...))->str:
     current_user_log_status["userid"]=userid
     current_user_log_status["logged"]=True
     return {"message":"Login successsful"}
+def get_user()->str:
+    return current_user_log_status["userid"]
+
+def user_logged()->bool:
+    user=get_user()
+    if not user:
+            raise HTTPException(status_code=403,detail="No user logged in")
+    if not current_user_log_status["logged"]:
+         return False
+    return True
+
+user_logged_dependency=Annotated[bool,Depends(user_logged)]
 
 def validate_pin(pin:str=Header(...)):
-    user_id=current_user_log_status["userid"]
-    if not user_id:
-        raise HTTPException(status_code=403,detail="No user logged in")
-    if pin != USER_PRIVATE_CREDENITIALS[user_id]["pin"]:
+    if pin != USER_PRIVATE_CREDENITIALS[get_user()]["pin"]:
         raise HTTPException(status_code=401,detail="Incorrect PIN")
     return {"message":"Transaction successful"}
 
@@ -27,10 +36,8 @@ pin_validation_dependency=Annotated[dict,Depends(validate_pin)]
 login_user_dependency=Annotated[dict,Depends(user_auth)]
 
 @app.get("/wallet/login")
-def login(user:login_user_dependency):
-    return {"message":"Welcome, Sir({user}). Your current balance is $999"}
-
-
+def login(res:login_user_dependency):
+    return {"message":f"Welcome, Sir({get_user()}). Your current balance is {USER_PRIVATE_CREDENITIALS[get_user()]['balance']}$"}
 
 
 
