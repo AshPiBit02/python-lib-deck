06d6a662c703f1dd0e4ddad22d2d8f99f77c4046
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-26 19:45:00 +0545

    feat: request counter throughout the project

--- FastAPI/Dependency Injection/Request Logger/request_logger.py
+++ FastAPI/Dependency Injection/Request Logger/request_logger.py
@@ -6,8 +6,13 @@ def verify_api_key(x_api_key:str=Header(...)):
     if x_api_key!="secret123":
         raise HTTPException(status_code=403,detail="Invalid or missing API key")
     
-app=FastAPI(dependencies=[Depends(verify_api_key)])
+request_count={}
+request_count["count"]=0
 
+def request_counter()->None:
+    request_count["count"]+=1
+
+app=FastAPI(dependencies=[Depends(verify_api_key),Depends(request_counter)])
 
 def get_request_id():
     return str(uuid.uuid4())
@@ -29,6 +34,10 @@ def get_user(user_id:int,db:dict=Depends(get_db),request_id:str=Depends(get_requ
     if not user:
         raise HTTPException(status_code=404,detail="User not found")
     return {"request_id":request_id,"user":user}
+
+@app.get("/request_count")
+def get_request_count():
+    return {"Count":request_count["count"]}
 
 @app.post("/users/{username}")
 def create_user(username:str,db:dict=Depends(get_db)):
