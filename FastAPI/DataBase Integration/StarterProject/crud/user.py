2de483d0e5e7bc43644331f39fcdfa63b0bc32ff
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-27 16:40:30 +0545

    feat: baisc crud operation defined

---
+++ FastAPI/DataBase Integration/StarterProject/crud/user.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from models.user import User
+
+def get_user(db:Session,user_id:int):
+    return db.query(User).filter(User.id==user_id).first()
+
+def get_user_by_email(db:Session,email:str):
+    return db.query(User).filter(User.email==email).first()
+
+def get_users(db:Session,skip:int=0,limit:int=100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def create_user(db:Session,name:str,email:str):
+    user=User(name=name,email=email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def update_user(db:Session,user_id,name=str):
+    user=db.query(User).filter(User.id==user_id).first()
+    if user is None:
+        return None
+    user.name=name
+    db.commit()
+    return user
+
+def delete_user(db:Session,user_id:int)->bool:
+    user=db.query(User).filter(User.id==user_id).first()
+    if user is None:
+        return False
+    db.delete(user)
+    db.commit()
+    return True
+
