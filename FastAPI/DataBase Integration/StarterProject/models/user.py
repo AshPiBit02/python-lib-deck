8053c9d119466a74b4a0ccf2fbe27bd3f9f1ca9a
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-27 16:29:01 +0545

    feat: first table created for users

--- FastAPI/DataBase Integration/StarterProject/models/user.py
+++ FastAPI/DataBase Integration/StarterProject/models/user.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column,Integer,String,Boolean
 from db.database import Base
-from pydantic import EmailStr
 
 class User(Base):
     __tablename__="users"
 
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String(100),nullable=False)
-    email=Column(EmailStr,unique=True,nullable=False)
+    email=Column(String(255),unique=True,nullable=False)
     is_active=Column(Boolean,default=True)
