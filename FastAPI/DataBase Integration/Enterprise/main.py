ce60d72c9a2fb1a0d9c13d3e74af28d5ca55d033
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-29 15:59:22 +0545

    feat: added secret key dependency which is required when manipuating the db

--- FastAPI/DataBase Integration/Enterprise/main.py
+++ FastAPI/DataBase Integration/Enterprise/main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI,Depends,HTTPException,APIRouter
+from fastapi import FastAPI,Depends,HTTPException,APIRouter,Header
 from sqlalchemy.orm import Session
 from db.database import get_db
 from typing import Annotated
 import crud.empCrud as Empcrud
 from BaseModels import EmpResponse,EmpAdd,EmpAddResponse,EmpSalaryResponse
 from sqlalchemy.exc import IntegrityError
+from core.config import settings
 
 app=FastAPI()
 enterprise_router=APIRouter(prefix="/enterprise")
 employee_router=APIRouter(prefix="/employee")
 
 database_dependency=Annotated[Session,Depends(get_db)]
+
+def key_validation(key:str=Header(...)):
+    if key!=settings.secret_key:
+        raise HTTPException(status_code=403,detail="Invalid secret key!")
+    
+secure_router=APIRouter(prefix="/employee",dependencies=[Depends(key_validation)])
 
 @employee_router.get("/view/list",response_model=list[EmpResponse])
 def employee_list(db:database_dependency):
@@ -20,14 +27,14 @@ def employee_list(db:database_dependency):
     return result
 
 
-@employee_router.get("/enterprise/employee/view/id/{id}",response_model=EmpResponse)
+@employee_router.get("/view/id/{id}",response_model=EmpResponse)
 def employee_by_id(db:database_dependency,id:int):
     emp=Empcrud.get_employees_by_id(db,id)
     if emp is None:
         raise HTTPException(status_code=404,detail=f"Employee with id {id} not found!")
     return emp
 
-@employee_router.post("/add", response_model=EmpAddResponse)
+@secure_router.post("/add", response_model=EmpAddResponse)
 def add_employee(db: database_dependency, emp: EmpAdd):
     try:
         return Empcrud.add_new_employee(db, emp)
@@ -58,7 +65,7 @@ def empolyee_by_salary(db:database_dependency,min:float,max:float):
         raise HTTPException(status_code=404,detail=f"No employee found with salary in range({min},{max})")
     return emp
 
-@employee_router.patch("/update/salary")
+@secure_router.patch("/update/salary")
 def update_employee_salary(db:database_dependency,emp_id:int,new_salary:float):
     emp=Empcrud.update_employee_salary(db,emp_id,new_salary)
     if not emp:
@@ -72,28 +79,28 @@ def paged_employee(db:database_dependency,skip:int,limit:int):
         raise HTTPException(status_code=404,detail="No employee found!")
     return emp
 
-@employee_router.patch("/update/department")
+@secure_router.patch("/update/department")
 def update_employee_department(db:database_dependency,emp_id:int,new_department:str):
     result=Empcrud.change_employee_department(db,emp_id,new_department)
     if not result["success"]:
         raise HTTPException(status_code=400,detail=result["error"])
     return {"message":result["message"]}
 
-@employee_router.delete("/delete/employee")
+@secure_router.delete("/delete/employee")
 def delete_employee(db:database_dependency,emp_id:int):
     result=Empcrud.remove_employee(db,emp_id)
     if not result["success"]:
             raise HTTPException(status_code=400,detail=result["error"])
     return {"message":result["message"]}
 
-@employee_router.patch("/update/status/deactivate")
+@secure_router.patch("/update/status/deactivate")
 def deactivate_employee(db:database_dependency,emp_id:int):
     result=Empcrud.deactivate_employee(db,emp_id)
     if not result["success"]:
         raise HTTPException(status_code=404,detail=result["error"])
     return {"message":result["message"]}
 
-@employee_router.patch("/update/status/reactivate")
+@secure_router.patch("/update/status/reactivate")
 def reactivate_employee(db:database_dependency,emp_id:int):
     result=Empcrud.reactivate_employee(db,emp_id)
     if not result["success"]:
@@ -108,4 +115,5 @@ def active_employee_list(db:database_dependency):
     return result
 
 enterprise_router.include_router(employee_router)
+enterprise_router.include_router(secure_router)
 app.include_router(enterprise_router)
