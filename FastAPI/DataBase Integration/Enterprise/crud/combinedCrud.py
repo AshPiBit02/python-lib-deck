1cfdd860722f2cc25ffa81dc0cfabba8d825afde
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-01 12:30:25 +0545

    bugfixed:imported valid function with correct name

--- FastAPI/DataBase Integration/Enterprise/crud/combinedCrud.py
+++ FastAPI/DataBase Integration/Enterprise/crud/combinedCrud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from models import Employee,Department,AggFunc
-from crud.empCrud import get_employees_by_id
+from crud.empCrud import get_employee_by_id
 from sqlalchemy import func
 from decimal import Decimal
 
@@ -27,7 +27,7 @@ def update_department_salary(db:Session,department:str,percentage:float):
         "updated_ids":[emp.id for emp in emps]}
 
 def change_employee_department(db: Session, emp_id: int, new_department: str):
-    emp = get_employees_by_id(db,emp_id)
+    emp = get_employee_by_id(db,emp_id)
     if not emp:
         return {"success":False,"error": f"Employee with id {emp_id} not found!"}
 
@@ -55,7 +55,7 @@ def total_salary_per_department(db:Session,agg:AggFunc):
         func_to_use=func.avg
         salary_type="average_salary"
     results=(
-        db.query(Department.name,func(Employee.salary).label("total_salary"))
+        db.query(Department.name,func_to_use(Employee.salary).label("total_salary"))
         .join(Employee,Department.id==Employee.department_id)
         .group_by(Department.id)
         .all()
