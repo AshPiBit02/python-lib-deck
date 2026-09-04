f6ee32a67c2398055387050f57ec88667070ec26
bsparrow49 <aashishchaudhari249@gmail.com>
2026-09-04 13:07:24 +0545

    feat(account_schemas): added missing config_dict()

--- FastAPI/DataBase Integration/Mobile Banking API/routers/account_router.py
+++ FastAPI/DataBase Integration/Mobile Banking API/routers/account_router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
+from typing import Annotated
 
 from db.database import get_db
-from services.account_s
+from services import create_customer
+
+database_dependency=Annotated[Session,Depends(get_db)]
